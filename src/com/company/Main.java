1b1f4210cca6b9ab9f43530b9c586efdcdae1aef
JorMoyano <you@example.com>
2022-08-23 20:27:30 -0300

    resolucion ejercicio 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
+
 package com.company;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Programa para indicar nombre y estado de estudios del alumno
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del alumno: ");
+        String studentName = myScanner.nextLine();
+
+        System.out.println("Ingrese la nota del primer examen: ");
+        int noteExamenOne = myScanner.nextInt();
+
+        System.out.println("Ingrese la nota del segundo examen: ");
+        int noteExamenTwo = myScanner.nextInt();
+
+        System.out.println("Ingrese la nota del tercer examen: ");
+        int noteExamenThree = myScanner.nextInt();
+
+        double averageNotes = (noteExamenOne + noteExamenTwo + noteExamenThree) / 3;
+
+        if (averageNotes < 1){
+            System.out.println("Error, promedio fuera de rango, revise las notas cargadas");
+        } else if (averageNotes < 6){
+            System.out.println("El alumno " + studentName + " esta desaprobado con promedio: " + averageNotes);
+        } else if (averageNotes < 9){
+            System.out.println("El alumno " + studentName + " esta aprobado con promedio: " + averageNotes);
+        } else if (averageNotes <= 10){
+            System.out.println("El alumno " + studentName + " esta promocionado con promedio: " + averageNotes);
+        } else {
+            System.out.println("Promedio fuera del rango permitido, revise las notas cargadas");
+        }
+
+
     }
 }
