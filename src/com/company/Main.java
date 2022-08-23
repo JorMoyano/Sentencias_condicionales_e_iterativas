015216e572e7b9a3a95dd8781af4297125eb346f
JorMoyano <you@example.com>
2022-08-23 20:33:52 -0300

    Resolucion ejercicio n° 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Programa para imprimir del 1 al 10, en lineas separadas
+        for (int number = 1; number < 11; number++) {
+            System.out.println(number);
+        }
     }
 }
