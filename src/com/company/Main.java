44b10d1c0b6c3840a3cba981b21385f04c0afaaa
JorMoyano <you@example.com>
2022-08-23 20:30:04 -0300

    Resolucion ejercicio 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Programa que imprime 10 veces la palabra "foo"
+        for (int i = 0; i < 10; i++) {
+            System.out.println("foo");
+        }
     }
 }
