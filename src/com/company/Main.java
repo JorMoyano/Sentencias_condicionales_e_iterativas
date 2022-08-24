4481ac98ed25ba6d01884e7a648e79ac32e7d356
JorMoyano <you@example.com>
2022-08-24 17:36:56 -0300

    Ejercicio n° 4 pasado a metodo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// Programa que imprime 10 veces la palabra "foo"
+        printLine();
+    }
+    public static void printLine(){
         for (int i = 0; i < 10; i++) {
             System.out.println("foo");
         }
