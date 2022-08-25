7aaa064d3cdbc103606a38e4267c4881ed6210db
JorMoyano <you@example.com>
2022-08-25 11:14:25 -0300

    Resolucion ejercicio extra n°2 Numeros primos entre 1 y n numero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Escriba un numero");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
+        for (int i = 1; i <= number; i++) {
+            if (isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+    private static boolean isPrime(int number){
+        boolean isPrime = true;
+        for (int i = 2; i < number ; i++) {
+            if (number % i == 0){
+                isPrime = false;
+            }
+        } return  isPrime;
     }
 }
